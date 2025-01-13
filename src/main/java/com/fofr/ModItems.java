a6e314d09e239348f82d09243f40ca2180f39c4d
Bobisawesome07 <bobisawesome07@outlook.com>
2025-01-13 18:37:43 +0000

    added sword tool

--- src/main/java/com/fofr/ModItems.java
+++ src/main/java/com/fofr/ModItems.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SwordItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -20,11 +21,17 @@ public class ModItems {
         return registeredItem;
     }
     public static final Item FRACTURED_SWORD = register(
-            new Item(new Item.Settings().maxCount(1)),
-            "fractured_sword"
+            new SwordItem(FracturedMaterial.INSTANCE, new Item.Settings(),
+            1.6)
 
     );
+    public static final Item FRACTURED_SHARD = register(
+            new Item(new Item.Settings().maxCount(64)),
+            "fractured_shard"
+    );
     public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModItems.FRACTURED_SHARD));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
                 .register((itemGroup) -> itemGroup.add(ModItems.FRACTURED_SWORD));
     }
