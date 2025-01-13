9b6b47eb82a2ab56544420897ce337e50b29730d
Bobisawesome07 <bobisawesome07@outlook.com>
2025-01-13 14:17:04 +0000

    Intial Commit

---
+++ src/client/java/com/fofr/FracturedModClient.java
@@ -0,0 +1,10 @@
+package com.fofr;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FracturedModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
