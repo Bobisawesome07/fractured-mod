07a8d80242eed2bdd6f4e561fd4cdd41f9b67946
Bobisawesome07 <bobisawesome07@outlook.com>
2025-01-13 16:12:02 +0000

    added sword texture and model

--- src/main/java/com/fofr/FracturedMod.java
+++ src/main/java/com/fofr/FracturedMod.java
@@ -12,10 +12,7 @@ public class FracturedMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
+		ModItems.initialize();
 		LOGGER.info("Fracture Mod Initialized");
 	}
 }
--- src/main/java/com/fofr/ModItems.java
+++ src/main/java/com/fofr/ModItems.java
@@ -1,6 +1,9 @@
 package com.fofr;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -15,5 +18,14 @@ public class ModItems {
 
         // Return the registered item!
         return registeredItem;
+    }
+    public static final Item FRACTURED_SWORD = register(
+            new Item(new Item.Settings().maxCount(1)),
+            "fractured_sword"
+
+    );
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                .register((itemGroup) -> itemGroup.add(ModItems.FRACTURED_SWORD));
     }
 }
