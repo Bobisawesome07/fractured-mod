0e55c0b1684f127ae42ac87cf2fd479de2e2672b
Bobisawesome07 <bobisawesome07@outlook.com>
2025-01-13 15:26:09 +0000

    made item creator class

---
+++ src/main/java/com/fofr/ModItems.java
@@ -0,0 +1,19 @@
+package com.fofr;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static Item register(Item item, String id) {
+        // Create the identifier for the item.
+        Identifier itemID = Identifier.of(FracturedMod.MOD_ID, id);
+
+        // Register the item.
+        Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
+
+        // Return the registered item!
+        return registeredItem;
+    }
+}
