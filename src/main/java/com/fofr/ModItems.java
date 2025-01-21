299f63ecb96989a3622b085ebd08a64df4be5f9c
Lucas Wood <lucaspwood@gmail.com>
2025-01-21 17:50:19 -0600

    please fix

--- src/main/java/com/fofr/ModItems.java
+++ src/main/java/com/fofr/ModItems.java
@@ -1,19 +1,21 @@
 package com.fofr;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.item.SwordItem;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+
+import static com.fofr.FracturedMod.MOD_ID;
 
 public class ModItems {
     public static Item register(Item item, String id) {
         // Create the identifier for the item.
-        Identifier itemID = Identifier.of(FracturedMod.MOD_ID, id);
+        Identifier itemID = Identifier.of(MOD_ID, id);
 
         // Register the item.
         Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
@@ -36,4 +38,20 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
                 .register((itemGroup) -> itemGroup.add(ModItems.FRACTURED_SWORD));
     }
+
+
+    public static final RegistryKey<ItemGroup> CUSTOM_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), new Identifier(MOD_ID, "item_group"));
+    public static final ItemGroup CUSTOM_ITEM_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.FRACTURED_SWORD))
+            .displayName(Text.translatable("Fractured Items"))
+            .build();
+
+    // Register the group.
+Registry.register(Registries.ITEM_GROUP, CUSTOM_ITEM_GROUP_KEY, CUSTOM_ITEM_GROUP);
+
+// Register items to the custom item group.
+ItemGroupEvents.modifyEntriesEvent(CUSTOM_ITEM_GROUP_KEY).register(itemGroup -> {
+        itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE);
+    });
+
 }
