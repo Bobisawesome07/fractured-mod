5e91b86046164aaa2089e4b93a10c3ff4f4691d6
LuketheFierce <lucaspwood@gmail.com>
2025-01-22 00:36:31 -0600

    • Huge organizational revamp done with/within classes. • New Item (Fractured Upgrade Template) • New Recipe (Smithing Table: FUT+Netherite_Sword+FS -> Fractured Sword). • New Loot_table json templates. • New Tooltip working for Fractured Sword. • Fixed Creative Inventory Tab. • Recreated fake_bedrock so as it copies the stone block. • Helper Methods added in to ModItems and ModBlocks. • Code does not crash game.

---
+++ src/main/java/com/fofr/block/ModBlocks.java
@@ -0,0 +1,38 @@
+package com.fofr.block;
+
+import com.fofr.FracturedMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+    // ---------- Declare a new block -------------------------------------------------------------------------------------------------- //
+    public static final Block FAKE_BEDROCK = registerBlock("fake_bedrock",
+            new Block(FabricBlockSettings.copyOf(Blocks.STONE)));
+
+
+
+
+
+
+    // --------- Helper Methods ------------------------------------------------------------------------------------------------------- //
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(FracturedMod.MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(FracturedMod.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
+
+    public static void registerBlocks() {
+        FracturedMod.LOGGER.info("Registering Mod Blocks for " + FracturedMod.MOD_ID);
+    }
+}
---
+++ src/main/java/com/fofr/item/custom/FracturedSwordItem.java
@@ -0,0 +1,35 @@
+package com.fofr.item.custom;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class FracturedSwordItem extends Item {
+    public FracturedSwordItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if(!world.isClient()) {
+            user.sendMessage(Text.literal("You opened a fractured portal!"));
+        }
+        return super.use(world, user, hand);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("item.fractured-mod.fractured_sword.tooltip").formatted(Formatting.GOLD).formatted(Formatting.ITALIC).formatted(Formatting.BOLD));
+        super.appendTooltip(stack, world, tooltip, context);
+    }
+}
