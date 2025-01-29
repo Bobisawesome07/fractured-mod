2ca7b3836b6e95d4dae9d8fc0a64e24ead877d77
Bobisawesome07 <100872835+Bobisawesome07@users.noreply.github.com>
2025-01-29 10:17:49 -0600

    added troll creature

--- src/main/java/com/fofr/FracturedMod.java
+++ src/main/java/com/fofr/FracturedMod.java
@@ -1,9 +1,16 @@
 package com.fofr;
 
 import com.fofr.block.ModBlocks;
+import com.fofr.entity.Troll;
 import com.fofr.item.ModItemGroups;
 import com.fofr.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,12 +19,19 @@ public class FracturedMod implements ModInitializer {
 	public static final String MOD_ID = "fractured-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final EntityType<Troll> TROLL = Registry.register(
+            Registries.ENTITY_TYPE,
+            Identifier.of("fratured-mod", "troll"),
+            EntityType.Builder.create(Troll::new, SpawnGroup.CREATURE).setDimensions(0.75f,0.8f).build("troll:")
+    );
+
 	@Override
 	public void onInitialize() {
 
     ModItemGroups.registerItemGroups();
 		ModItems.registerItems();
 		ModBlocks.registerBlocks();
+		FabricDefaultAttributeRegistry.register(TROLL, Troll.createMobAttributes());
     
 		LOGGER.info("Fracture Mod Initialized");
 
