a05892e10fd2a61a03e6943e1529f7279776e115
Bobisawesome07 <100872835+Bobisawesome07@users.noreply.github.com>
2025-01-31 09:04:32 -0600

    added cooldown for the sword (ty mojang for itemCooldownManager)

--- src/main/java/com/fofr/item/custom/FracturedSwordItem.java
+++ src/main/java/com/fofr/item/custom/FracturedSwordItem.java
@@ -1,6 +1,8 @@
 package com.fofr.item.custom;
 
+import com.fofr.item.ModItems;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.ItemCooldownManager;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -8,20 +10,25 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.HashMap;
 
 public class FracturedSwordItem extends Item {
+    
+    private void resetCooldown(PlayerEntity user, Item sword){
+        user.getItemCooldownManager().set(sword, 300);
+    }
     public FracturedSwordItem(Settings settings) {
         super(settings);
     }
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if(!world.isClient()) {
+        if(!world.isClient()&&!user.getItemCooldownManager().isCoolingDown(this.asItem())) {
+
             user.sendMessage(Text.literal("You opened a fractured portal!"));
         }
         return super.use(world, user, hand);
