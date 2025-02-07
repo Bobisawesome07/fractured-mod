d647c2d1e0b589eb58bb835601bb95afdf92073c
Bobisawesome07 <100872835+Bobisawesome07@users.noreply.github.com>
2025-02-07 16:28:23 -0600

    made it gen a void world todo - add non owner player handler - test

--- src/main/java/com/fofr/world/dimension/ModDimensions.java
+++ src/main/java/com/fofr/world/dimension/ModDimensions.java
@@ -4,6 +4,8 @@ import com.fofr.FracturedMod;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.BiomeTags;
 import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
@@ -14,6 +16,7 @@ import net.minecraft.world.dimension.DimensionTypes;
 import xyz.nucleoid.fantasy.Fantasy;
 import xyz.nucleoid.fantasy.RuntimeWorldConfig;
 import xyz.nucleoid.fantasy.RuntimeWorldHandle;
+import xyz.nucleoid.fantasy.util.VoidChunkGenerator;
 
 import static com.fofr.FracturedMod.MOD_ID;
 
@@ -24,11 +27,15 @@ public class ModDimensions {
     public static final RegistryKey<DimensionType> POCKET_DIM_TYPE = RegistryKey.of(RegistryKeys.DIMENSION_TYPE,
           new Identifier(MOD_ID, "pocket_dimension"));
     static Fantasy fantasy = Fantasy.get(FracturedMod.getServer());
+    private static VoidChunkGenerator pocketGen = new VoidChunkGenerator(
+            FracturedMod.getServer().getRegistryManager().get(RegistryKeys.BIOME),
+            RegistryKey.of(RegistryKeys.BIOME, new Identifier("minecraft", "the_end"))
+    );
     static RuntimeWorldConfig worldConfig = new RuntimeWorldConfig()
             .setDimensionType(ModDimensions.POCKET_DIM_TYPE)
             .setDifficulty(Difficulty.HARD)
             .setGameRule(GameRules.DO_DAYLIGHT_CYCLE, false)
-            .setGenerator(FracturedMod.getServer().getOverworld().getChunkManager().getChunkGenerator())
+            .setGenerator(pocketGen)
             .setSeed(1234L);
 
     public static void createOrLoadPocketDimension(String nameSpace, String uuid){
