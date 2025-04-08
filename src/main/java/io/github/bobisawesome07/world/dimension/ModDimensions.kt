16f077219ca0f7f3b118bc06f423c5f17cb4cef3
Bobisawesome07 <100872835+Bobisawesome07@users.noreply.github.com>
2025-04-08 09:38:37 -0500

    whoops i did it again

--- src/main/java/io/github/bobisawesome07/world/dimension/ModDimensions.kt
+++ src/main/java/io/github/bobisawesome07/world/dimension/ModDimensions.kt
@@ -6,21 +6,16 @@ import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.registry.Registerable
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.RegistryKeys
-import net.minecraft.registry.tag.BlockTags
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.Identifier
-import net.minecraft.util.math.intprovider.UniformIntProvider
 import net.minecraft.world.Difficulty
 import net.minecraft.world.GameRules
 import net.minecraft.world.World
 import net.minecraft.world.biome.BiomeKeys
 import net.minecraft.world.dimension.DimensionType
-import net.minecraft.world.dimension.DimensionType.MonsterSettings
-import net.minecraft.world.dimension.DimensionTypes
 import xyz.nucleoid.fantasy.Fantasy
 import xyz.nucleoid.fantasy.RuntimeWorldConfig
 import xyz.nucleoid.fantasy.RuntimeWorldHandle
-import java.util.*
 import java.util.concurrent.ConcurrentHashMap
 
 object ModDimensions {
@@ -96,24 +91,10 @@ object ModDimensions {
      * @param context Registry context
      */
     fun bootstrapType(context: Registerable<DimensionType?>?) {
-        context.register(
-            POCKET_DIM_TYPE, DimensionType(
-                OptionalLong.of(0),  // Fixed time
-                false,  // Skylight
-                false,  // Ceiling
-                false,  // Ultra warm
-                false,  // Natural
-                1.0,  // Coordinate scale
-                false,  // Bed works
-                false,  // Respawn anchor works
-                0,  // Min Y
-                256,  // Height
-                256,  // Logical height
-                BlockTags.INFINIBURN_OVERWORLD,
-                DimensionTypes.OVERWORLD_ID,
-                1.0f,
-                MonsterSettings(false, false, UniformIntProvider.create(0, 0), 0)
-            )
-        )
+        register()
     }
+}
+
+private fun register() {
+
 }
